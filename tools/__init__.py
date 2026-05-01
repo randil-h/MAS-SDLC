a59137f3d6b74266cc9a165236a61d7d6455e7e3
randil-h <51442488+randil-h@users.noreply.github.com>
2026-05-02 01:23:29 +0530

    added code reviewer tests and review tools

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,9 +1,10 @@
 """
 MAS SDLC tools package.
 
-Exposes the four tool modules:
+Exposes the five tool modules:
   - file_tools      : save_to_file, read_from_file, append_log
   - execution_tools : run_pytest
   - analysis_tools  : run_static_analysis (flake8)
   - code_tools      : validate_python_code, strip_markdown_fences
+  - review_tools    : parse_review_sections, ReviewSections
 """
